54906f75237f39811b3b81137fd0e1dc51078aea
maichakWesley <72577791+maichakWesley@users.noreply.github.com>
2023-07-11 16:03:15 -0300

    aula38 e 39

--- Aula38/Program.cs
+++ Aula38/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aula29 //File, FileInfo
+/*File, FileInfo
+• Namespace System.IO
+• Realiza operações com arquivos (create, copy, delete, move, open, etc.) e
+ajuda na criação de objetos FileStream.
+• File
+• static members (simples, mas realiza verificação de segurança para cada operação)
+• https://msdn.microsoft.com/en-us/library/system.io.file(v=vs.110).aspx
+• FileInfo
+• instance members
+• https://msdn.microsoft.com/en-us/library/system.io.fileinfo(v=vs.110).aspx
+
+IOException
+Namespace System.IO
+• IOException
+• DirectoryNotFoundException
+• DriveNotFoundException
+• EndOfStreamException
+• FileLoadException
+• FileNotFoundException
+• PathTooLongException
+• PipeException
+
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string sourcePath = @"C:\Users\mie3ct\Desktop\undemyC#\Curso-C-\file1.txt";
+            string targetPath = @"C:\Users\mie3ct\Desktop\undemyC#\Curso-C-\file2.txt";
+
+            try{
+
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                //fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines){
+                    Console.WriteLine(line);
+                }
+
+            }catch(IOException e){
+                Console.WriteLine("An erro occurred");
+                 Console.WriteLine(e.Message);
+            }
+
+            
+        }    
+    }
+}
+
---
+++ Aula39/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aula29 //Strem Reader
+/*
+FileStream
+https://msdn.microsoft.com/en-us/library/system.io.filestream(v=vs.90).aspx
+Disponibiliza uma stream associada a um arquivo, permitindo operações de leitura
+e escrita.
+Suporte a dados binários.
+Instanciação:
+• Vários construtores
+• File / FileInfo
+
+read bytes---------------FileStream
+write bytes----------´´
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = @"C:\Users\mie3ct\Desktop\undemyC#\Curso-C-\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try{
+                //abrindo stream
+                
+                sr = File.OpenText(path);
+
+               
+                while(!sr.EndOfStream){
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                
+
+            }catch(IOException e){
+                Console.WriteLine("An erro occurred");
+                 Console.WriteLine(e.Message);
+            }finally{//fechando stream
+
+                if(sr != null) sr.Close();
+             
+
+            }
+
+            
+        }    
+    }
+}
+
+
